014ff32aba876ebf6377576e0d2684cc920387f0
israil64 <md3096720@gmail.com>
2025-12-26 18:02:13 +0530

    add .

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', initApp);
    =========================== */
 // Define laptops globally so both renderInventory and openModal can access it
 let laptops = []; 
-const API_BASE = 'http://localhost:3000/api'; // Point to your backend
+const API_BASE = 'https://laptop-galaxy-backend.onrender.com/api'; // Point to your backend
 
 
 
